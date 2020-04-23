5a288fb0367ddb58b42fcd6d03aca34cf2ba84c2
Angkor.Wu <wjkhappy14@hotmail.com>
2020-04-23 11:56:48 +0800

    空间类型 - geography
    https://docs.microsoft.com/zh-cn/sql/t-sql/spatial-geography/spatial-types-geography?view=sql-server-ver15

--- src/Abp.VNext.Hello.Domain/City/City.cs
+++ src/Abp.VNext.Hello.Domain/City/City.cs
@@ -14,6 +14,7 @@ namespace Abp.VNext.Hello
         public virtual string ChineseName { get; set; }
         public virtual int StateProvinceId { get; set; }
 
+        //https://docs.microsoft.com/zh-cn/sql/t-sql/spatial-geography/spatial-types-geography?view=sql-server-ver15
         [JsonIgnore]
         public Geometry Location { get; set; }
 
