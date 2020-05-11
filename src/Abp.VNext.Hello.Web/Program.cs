8e07b0a2f73829e2f8efc03887c7c1dae950c001
Administrator <Administrator@BAIDU-DEV>
2020-05-11 14:53:25 +0800

    webHostBuilder.ConfigureKestrel(serverOptions =>{})

--- src/Abp.VNext.Hello.Web/Program.cs
+++ src/Abp.VNext.Hello.Web/Program.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Abp.VNext.Hello.XNetty.Server;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -14,17 +16,15 @@ namespace Abp.VNext.Hello.Web
         public static async Task<int> Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-#if DEBUG
                 .MinimumLevel.Debug()
-#else
-                .MinimumLevel.Information()
-#endif
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
                 .Enrich.FromLogContext()
-                .WriteTo.Async(c => c.File("Logs/logs.txt"))
-                .CreateLogger();
 
+                .WriteTo.Logger(lc => lc
+                .WriteTo.Async(c => c.File("Logs/logs.txt")))
+
+                .CreateLogger();
             try
             {
                 Log.Information("Starting web host.");
@@ -46,12 +46,38 @@ namespace Abp.VNext.Hello.Web
 
         internal static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webHostBuilder =>
+            .ConfigureServices((context, services) =>
+            {
+                services.Configure<KestrelServerOptions>(context.Configuration.GetSection("Kestrel"));
+            })
+            .ConfigureWebHostDefaults(webHostBuilder =>
                 {
                     webHostBuilder.PreferHostingUrls(true);
                     webHostBuilder.UseStaticWebAssets();
+
                     webHostBuilder.UseUrls(url);
+                    //Kestrel https://docs.microsoft.com/zh-cn/aspnet/core/fundamentals/servers/kestrel?view=aspnetcore-3.1
+                    webHostBuilder.ConfigureKestrel(serverOptions =>
+                     {
+                         //为整个应用设置并发打开的最大 TCP 连接数：
+                         serverOptions.Limits.MaxConcurrentConnections = 100;
+                         serverOptions.Limits.MaxConcurrentUpgradedConnections = 100;
+                         serverOptions.Limits.MaxRequestBodySize = 10 * 1024;
+
+                         serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(2);
+                         serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(1);
+
+                     });
                     webHostBuilder.UseStartup<Startup>();
+
+                    webHostBuilder.UseKestrel(opts =>
+                    {
+                        //https://andrewlock.net/5-ways-to-set-the-urls-for-an-aspnetcore-app/
+                        //opts.ListenAnyIP(8080, x =>
+                        //{
+
+                        //});
+                    });
                 })
                 .UseAutofac()
                 .UseSerilog();
