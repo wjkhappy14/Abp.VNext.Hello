e644d86096fc6476d120abdcc4faae4acc471f14
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2020-08-06 18:02:50 +0800

    Cors  & AcceptVerbs

--- src/Abp.VNext.Hello.HttpApi/Controllers/HelloController.cs
+++ src/Abp.VNext.Hello.HttpApi/Controllers/HelloController.cs
@@ -1,7 +1,10 @@
 ﻿using Abp.VNext.Hello.Localization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
+using System;
+using System.Collections;
 using Volo.Abp.AspNetCore.Mvc;
 
 namespace Abp.VNext.Hello.Controllers
@@ -13,11 +16,19 @@ namespace Abp.VNext.Hello.Controllers
         {
             LocalizationResource = typeof(HelloResource);
         }
-
+        [EnableCors("WithCredentials")]
+        [HttpOptions]
+        public IDictionary Variables()
+        {
+            IDictionary variables = Environment.GetEnvironmentVariables();
+            return variables;
+        }
         /// <summary>
         /// 限制请求正文
         /// </summary>
         /// <returns></returns>
+        [DisableCors]
+        [AcceptVerbs("HEAD", "GET", "POST")]
         [RequestSizeLimit(100000000)]
         public IActionResult Hello()
         {
