604694a0a0f1537620d947b63dbcf84d270245a6
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2020-11-02 13:21:50 +0800

    ...

---
+++ src/Abp.VNext.Hello.Web/MyOpenIdConnectHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Abp.VNext.Hello
+{
+
+    [Dependency(ReplaceServices = true)]
+    public class MyOpenIdConnectHandler : OpenIdConnectHandler
+    {
+
+        public MyOpenIdConnectHandler(IOptionsMonitor<OpenIdConnectOptions> options, ILoggerFactory logger, HtmlEncoder htmlEncoder, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, htmlEncoder, encoder, clock)
+        {
+
+
+        }
+
+        protected override bool ValidateCorrelationId(AuthenticationProperties properties)
+        {
+            return base.ValidateCorrelationId(properties);
+        }
+
+        protected override Task<HandleRequestResult> HandleRemoteAuthenticateAsync()
+        {
+            return base.HandleRemoteAuthenticateAsync();
+        }
+    }
+}
