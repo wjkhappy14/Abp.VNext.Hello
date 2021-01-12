e2d167a1a2014288140b734c0540bccea216daa0
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2021-01-12 17:48:35 +0800

    Version="4.1.0"

--- src/Abp.VNext.Hello.Web/HelloBrandingProvider.cs
+++ src/Abp.VNext.Hello.Web/HelloBrandingProvider.cs
@@ -1,11 +1,10 @@
-﻿using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared.Components;
-using Volo.Abp.DependencyInjection;
+﻿using Volo.Abp.DependencyInjection;
 
 namespace Abp.VNext.Hello.Web
 {
     [Dependency(ReplaceServices = true)]
-    public class HelloBrandingProvider : DefaultBrandingProvider
+    public class HelloBrandingProvider// : DefaultBrandingProvider
     {
-        public override string AppName => "Hello";
+        public  string AppName => "Hello";
     }
 }
