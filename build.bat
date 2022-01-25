e3e3143381e23c1b24326bb243ba2f8ee371b949
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2022-01-25 17:38:24 +0800

    ...

---
+++ build.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+
+echo "Windows Docker build"
+
+cd ../LintSense.Exam.HttpApi.Host
+
+dotnet publish -c Release -o ../publish
+
+cd ../publish
+
+echo "发布成功"
+
+docker build -t lintense.exam.httpapi.host .
