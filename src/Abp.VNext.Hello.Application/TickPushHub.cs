97439ce8ced847c5a5b1f06f1fe19a856c77dc91
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2022-05-19 17:02:01 +0800

    TickPushConnectionHandler

--- src/Abp.VNext.Hello.Application/TickPushHub.cs
+++ src/Abp.VNext.Hello.Application/TickPushHub.cs
@@ -1,9 +1,7 @@
-﻿using AutoMapper.Configuration;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using StackExchange.Redis;
 using System;
-using System.Net;
 using System.Threading.Tasks;
 using Volo.Abp.AspNetCore.SignalR;
 
@@ -19,11 +17,11 @@ namespace Abp.VNext.Hello
             _logger = logger;
             config = configuration;
             _logger.Log(LogLevel.Information, "Tick Push Hub");
-            Start();
         }
 
         public override Task OnConnectedAsync()
         {
+           
             return base.OnConnectedAsync();
         }
         public override Task OnDisconnectedAsync(Exception exception)
@@ -31,35 +29,17 @@ namespace Abp.VNext.Hello
             return base.OnDisconnectedAsync(exception);
         }
 
-        private static ConnectionMultiplexer Redis => ConnectionMultiplexer.Connect(new ConfigurationOptions
+      
+        public async Task Send(string message)
         {
-            Password = "03hx5DDDivYmbkTgDlFz",
-            EndPoints = {
-                new IPEndPoint(IPAddress.Parse("117.50.40.186"), 6379)
-            }
-        });
-        private void Start()
-        {
-            Subscribe("CME:EC:2206");
-            Subscribe("HKEX:HSI:2205");
+            message = $"{DateTime.Now}: {message}";
+            await Clients.All
+                .SendAsync("ReceiveMessage", message);
         }
-        private void Subscribe(string channelName)
+
+        public Task Publish(string message)
         {
-            ISubscriber sub = Redis.GetSubscriber();
-            sub.Subscribe(channelName, async (channel, message) =>
-            {
-                long id = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                string item = message.ToString();
-                string[] values = item.Split(',');
-                string timestamp = values[0];
-                string qlastprice = values[1];
-                string qlastqty = values[2];
-                _logger.Log(LogLevel.Information, message);
-                if (Clients != null)
-                {
-                    await Clients.All.SendAsync("responseHandler", message);
-                }
-            });
+            return this.Clients.All.SendAsync("responseHandler", message);
         }
     }
 }
