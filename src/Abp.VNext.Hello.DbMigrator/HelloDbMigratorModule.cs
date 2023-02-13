6a6a9242b4b3a27cf9d093ab9998ae6eee2055cc
AngkorW <AngkorW@ANGKORW-DEV>
2023-02-13 17:46:22 +0800

    AbpDbContextOptions

--- src/Abp.VNext.Hello.DbMigrator/HelloDbMigratorModule.cs
+++ src/Abp.VNext.Hello.DbMigrator/HelloDbMigratorModule.cs
@@ -1,6 +1,8 @@
 ﻿using Abp.VNext.Hello.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Autofac;
 using Volo.Abp.BackgroundJobs;
+using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.Modularity;
 
 namespace Abp.VNext.Hello.DbMigrator
@@ -15,6 +17,10 @@ namespace Abp.VNext.Hello.DbMigrator
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+            context.Services.Configure<AbpDbContextOptions>((o) =>
+            {
+                o.UseSqlServer((s) => { });
+            });
         }
     }
 }
